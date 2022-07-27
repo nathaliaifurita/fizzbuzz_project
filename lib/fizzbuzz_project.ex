d64fea3d7d43a7956df74f80b8b416c862553f51
Nathalia Freire <nathaliaofreire@gmail.com>
2022-07-27 10:28:33 -0300

    Removing the comments

--- lib/fizzbuzz_project.ex
+++ lib/fizzbuzz_project.ex
@@ -1,36 +1,36 @@
 defmodule FizzbuzzProject do
- def build(filename) do
+  def build(filename) do
     filename
     |> File.read()
-    |> handle_file_read()  
- end
+    |> handle_file_read()
+  end
 
- defp handle_file_read({:ok, result}) do
-  list = String.split(result, ",")
-  # Enum.map(list, fn number -> String.to_integer(number) end)
- end
+  #  defp handle_file_read({:ok, result}) do
+  #   list = String.split(result, ",")
+  #   Enum.map(list, fn number -> String.to_integer(number) end)
+  #  end
 
- defp handle_file_read({:error, reason}), do: reason
+  #  defp handle_file_read({:error, reason}), do: reason
 
- #  defp handle_file_read({:ok, result}) do
-#    result = 
-#    result
-#    |> String.split(",")
-#    |> Enum.map(&convert_and_evaluate_numbers/1)
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
 
-#    {:ok, result}
-#  end
+    {:ok, result}
+  end
 
-#  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
-   
-#  defp convert_and_evaluate_numbers(elem) do
-#   elem 
-#   |> String.to_integer()
-#   |>evaluate_numbers()
-#  end
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 
-#  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
-#  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
-#  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
-#  defp evaluate_numbers(number), do: number
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
